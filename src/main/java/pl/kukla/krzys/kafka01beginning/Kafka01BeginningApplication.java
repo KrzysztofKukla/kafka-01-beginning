86f0089b8a4a2441751f351b58b2c199cb7ae7a2
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-31 22:28:37 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/kafka01beginning/Kafka01BeginningApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.kafka01beginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka01BeginningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafka01BeginningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/kafka01beginning/Kafka01BeginningApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.kafka01beginning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka01BeginningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
